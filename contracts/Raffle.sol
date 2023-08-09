6349913db872dbcd2c2e5acc19d17b5059effec8
BerkeAnna <berke.anna0803@gmail.com>
2023-08-09 04:04:24 +0200

    wrong

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -10,7 +10,14 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 error Raffle__NotEnoughETHEntered();  
 error Raffle__TransferFailed();
 error Raffle_NotOpen();
+error Raffle__UpkeepNotNeeded(uint256 currenctBalance, uint256 numPlayers, uint256 raffleState);
 
+
+/**@title A sample Raffle Contract
+ * @author Berke Anna
+ * @notice This contract is for creating a sample raffle contract
+ * @dev This implements the Chainlink VRF Version 2
+ */
 contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface{
 
     //type declaration
@@ -33,6 +40,7 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface{
     address private s_recentWinner;
     RaffleState private s_raffleState;
     uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
 
 
 
@@ -41,8 +49,15 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface{
     event RequestedRaffleWinner(uint256 indexed requestId);
     event WinnerPicked(address indexed winner);
 
-
-    constructor(address vrfCoordinatorV2, uint256 entranceFee, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    //functions
+    constructor(
+        address vrfCoordinatorV2, 
+        uint256 entranceFee, 
+        bytes32 gasLane, 
+        uint64 subscriptionId, 
+        uint32 callbackGasLimit,
+        uint256 interval
+        ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
@@ -50,6 +65,7 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface{
         i_callbackGasLimit = callbackGasLimit;
         s_raffleState = RaffleState.OPEN;
         s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
     }
   
     function enterRaffle() public payable{
@@ -67,12 +83,19 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface{
      * @dev  
      */
 
-    function checkUpKeep(bytes calldata /*checkData*/) external override {
+    function checkUpKeep(bytes memory /*checkData*/) public /*override*/ returns(bool upkeepNeeded, bytes memory /* performData*/){
             bool isOpen = RaffleState.OPEN == s_raffleState;
-
+            bool timePassed = ((block.timestamp - i_interval) > i_interval);
+            bool hasPlayers = (s_players.length > 0);
+            bool hasBalance = address(this).balance > 0;
+            upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
     }
 
-    function requestRandomWinner() external {
+    function performUpkeep(bytes calldata /* performData */ ) external override {
+        (bool upkeepNeeded, ) = checkUpKeep(""); 
+        if(!upkeepNeeded){
+            revert Raffle__UpkeepNotNeeded( address(this).balance, s_players.length, uint256(s_raffleState));
+        }
 
         s_raffleState = RaffleState.CALCULATING;
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
@@ -91,6 +114,7 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface{
         s_recentWinner = recentWinner;
         s_raffleState = RaffleState.OPEN;
         s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
         if(!success){
             revert Raffle__TransferFailed();
@@ -99,6 +123,7 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface{
     }
 
 
+    //view / pure functions
     function getEntranceFee() public view returns(uint256){
         return i_entranceFee;
     }
@@ -109,6 +134,14 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface{
 
     function hetRecentWinner() public view returns(address){
         return s_recentWinner;  
+    }
+
+    function getRaffleState() public view returns(RaffleState){
+        return s_raffleState;
+    }
+
+    function getNumWords() public view returns(uint256){
+        return NUM_WORDS;
     }
 
 }
