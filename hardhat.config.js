bf8b8a66f97af2fc8e5d573aae01c8e9e81ffafc
BerkeAnna <berke.anna0803@gmail.com>
2023-08-09 04:39:03 +0200

    missing .env

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,8 +6,39 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
+
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY 
+const SEPOLIA_RPC_URL = process.env.RPC_URL || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
+      saveDeployments: true,
+      chainId: 11155111,
+  },
+  },
   solidity: "0.8.19",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
 };
   
