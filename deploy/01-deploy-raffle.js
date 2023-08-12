35c770a22060b346c2170b52da6eb2fd64d8b382
BerkeAnna <berke.anna0803@gmail.com>
2023-08-12 11:51:19 +0200

    without commits

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -15,15 +15,10 @@ module.exports = async ({ getNamedAccounts, deployments}) => {
         vrfCoordinatorV2Address = VrfCoordinatorV2Mock.target
         const transactionResponse = await VrfCoordinatorV2Mock.createSubscription()
         const transactionReceipt = await transactionResponse.wait()
-        console.log("---------..................-----------------")
+        
         console.log(transactionReceipt.logs[0].args.subId)
-        console.log("---------..................-----------------")
         
         subscriptionId = transactionReceipt.logs[0].args.subId
-        console.log("---------.............subscriptionId.....-----------------")
-        console.log(subscriptionId)
-        console.log("---------..................-----------------")
-        console.log(VrfCoordinatorV2Mock.target)
         await VrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT )
     } else{
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
@@ -35,16 +30,7 @@ module.exports = async ({ getNamedAccounts, deployments}) => {
     const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
     //const interval = networkConfig[chainId]["interval"]
     const interval = 30
-    /*
-    console.log("---------------------------------------------------------------------intevdas")
-    console.log( networkConfig[chainId])
-
-    console.log("---------------------------------------------------------------------das")
-    console.log(entranceFee)
-    console.log(gasLane)
-    console.log(callbackGasLimit)
-    console.log(interval)
-    console.log(vrfCoordinatorV2Address)*/
+    
     const waitBlockConfirmations = developmentChains.includes(network.name) ? 1: VERIFICATION_BLOCK_CONFIRMATIONS
 
     const args = [
@@ -55,10 +41,7 @@ module.exports = async ({ getNamedAccounts, deployments}) => {
         entranceFee,
         callbackGasLimit
      ]
-    
-   // console.log("---------------------------------------------------------------------das")
-   // console.log(waitBlockConfirmations)
-  
+
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: args,
@@ -66,9 +49,6 @@ module.exports = async ({ getNamedAccounts, deployments}) => {
         waitConfirmation: waitBlockConfirmations,
         
     }) 
-  //  console.log("---------------------------------------------------------------------3")
-   // console.log(raffle)
-
 
     if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY){
         log("Verifying ..")
